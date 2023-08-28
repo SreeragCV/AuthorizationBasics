f2a12359beeca22494a07652225184e5f60111aa
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-08-28 21:35:30 +0530

    Registering

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/user');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://127.0.0.1:27017/authdemo')
 .then(() => {
@@ -18,8 +19,23 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('THIS IS HOME PAGE!!!!')
+})
+
 app.get('/register', (req, res) => {
     res.render('register');
+})
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secrets', (req, res) => {
